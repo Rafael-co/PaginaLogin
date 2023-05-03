d032ca7b69caadcdce088f2a65cc78164ca56d44
Rafael-co <rafael.camilo21@outlook.com>
2023-05-03 17:49:55 -0300

    Adicião do script de mysql

---
+++ SQL/script mysql.sql
@@ -0,0 +1,11 @@
+create database login;
+use login;
+create TABLE usuario (
+ id_usuario INT AUTO_INCREMENT PRIMARY key,
+ nome varchar(45),
+ Cpf varchar(45),
+ email VARCHAR(45),
+ senha varchar(45),
+ telefone VARCHAR(45)
+) ;
+
